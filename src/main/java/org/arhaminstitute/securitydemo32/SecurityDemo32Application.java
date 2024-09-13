0b4d7003f59190045e13e21d736ed17a81ea1f5c
ViralSir <viralsir2018@gmail.com>
2024-09-13 12:36:07 +0530

    Initial commit

---
+++ src/main/java/org/arhaminstitute/securitydemo32/SecurityDemo32Application.java
@@ -0,0 +1,13 @@
+package org.arhaminstitute.securitydemo32;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemo32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemo32Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/arhaminstitute/securitydemo32/SecurityDemo32ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arhaminstitute.securitydemo32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemo32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
